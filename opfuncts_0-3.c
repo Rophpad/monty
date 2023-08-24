d47b8e6e136c5eacdd2a1ec76f3bb8cc59d40851
Rophpad <rophenp@gmail.com>
2023-08-24 10:00:42 -0700

    compile files

--- opfuncts_0-3.c
+++ opfuncts_0-3.c
@@ -114,10 +114,10 @@ void swap(stack_t **stack, unsigned int line_number)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n",
 				line_number);
-		gree_all();
+		free_all();
 		exit(EXIT_FAILURE);
 	}
 	temp = (*stack)->n;
 	(*stack)->n = (*stack)->next->n;
-	(*stack)->next->n = tmp;
+	(*stack)->next->n = temp;
 }
