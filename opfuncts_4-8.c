8ae59f13c021bd3f166df9c88c42217cb7490a73
Rophpad <rophenp@gmail.com>
2023-08-24 15:06:38 -0700

    div nd mul opcode implemented

--- opfuncts_4-8.c
+++ opfuncts_4-8.c
@@ -39,3 +39,49 @@ void sub(stack_t **stack, unsigned int line_number)
 	s->next->n = s->next->n - s->n;
 	pop(stack, line_number);
 }
+
+/**
+ * _div - divide the top two elements of the stack
+ * @stack: Double linked list
+ * @line_number: File line counter
+ */
+void _div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *s = *stack;
+
+	if (!s || !s->next)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n",
+				line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	if (s->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	s->next->n = s->next->n / s->n;
+	pop(stack, line_number);
+}
+
+/**
+ * mul - s
+ * @stack: Double linked list
+ * @line_number: File line execution
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *s = *stack;
+
+	if (!s || !s->next)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n",
+				line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	s->next->n = s->next->n * s->n;
+	pop(stack, line_number);
+}
